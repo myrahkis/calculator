cc312775658e59855269acb5bceda5145c2f3bf4
Mira <kruzinka@yandex.ru>
2024-07-13 14:31:10 +0300

    made all the buttons

--- src/components/Numbers.jsx
+++ src/components/Numbers.jsx
@@ -1,7 +1,22 @@
+import styles from './numbers.module.css';
+
 function Numbers() {
     return (
-      <div>
-        <p>Цифры</p>
+      <div className={styles['numbers-btns']}>
+        <button>←</button>
+        <button>CE</button>
+        <button>C</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className={styles['zero']}>0</button>
+        <button>.</button>
       </div>
     );
   }
--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -3,15 +3,15 @@ import styles from "./operations.module.css";
 function Operations() {
     return (
       <div className={styles['operations-btns']}>
-        <p className={styles['negate']}>+_</p>
-        <p className={styles['divide']}>/</p>
-        <p className={styles['multiply']}>*</p>
-        <p className={styles['minus']}>-</p>
-        <p className={styles['plus']}>+</p>
-        <p className={styles['root']}>корень</p>
-        <p className={styles['remainder']}>%</p>
-        <p className={styles['reciprocal']}>1/x</p>
-        <p className={styles['result']}>=</p>
+        <button className={styles['negate']}>±</button>
+        <button className={styles['divide']}>/</button>
+        <button className={styles['multiply']}>*</button>
+        <button className={styles['minus']}>−</button>
+        <button className={styles['plus']}>+</button>
+        <button className={styles['root']}>√</button>
+        <button className={styles['remainder']}>%</button>
+        <button className={styles['reciprocal']}>1/x</button>
+        <button className={styles['result']}>=</button>
       </div>
     );
   }
